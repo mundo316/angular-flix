ba94dddf5c99297e1e9fe2194078861b60a34559
Jordan Woodruff <boogermanus@gmail.com>
2020-02-03 22:23:01 -0600

    ng generate service services/Api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from 'selenium-webdriver';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiServiceService {
+export class ApiService {
 
   constructor() { }
 }
